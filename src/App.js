0c574211c2d38b6ddf1337a9944ecce00b02a705
Aklar45dev <aklar45dev@gmail.com>
2022-08-23 13:57:58 -0400

    fautes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ $(() => {
 
 })
   
-  const chapter = [{audioAns: ["https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-1.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-2.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-3.mp3"], audioQuestion: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-0.wav", question: "Quel aurait été la bonne attitude à adapté lors de la réunion du début de quart de travail ?", audio: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/paragraphe+1.wav", url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-2.mp4', choice1: 'Souhaitant clarifier ses incompréhensions rapidement, il intervient immédiatement dans le groupe afin de demander des précisions.', choice2: 'À la sortie, il demande au superviseur quand sera le bon moment pour lui parler avant la tâche. Le superviseur lui dit qu’il passera le voir rapidement. Il lui posera alors ses questions.', choice3: 'Il attend que la rencontre se termine et va valider les informations avec le superviseur lorsque tout le monde se dirige vers leur tâche à accomplir.', text: ["• Assurer votre sécurité et celles de vos collègues", "• Poser des questions", "• Choisissez le bon moment", "• Favoriser le dialogue"]},
-                  {audioAns: ["https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-1.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-2.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-3.mp3"], audioQuestion: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-0.mp3", question: "Quel aurait été le bon comportement a adopté pour cette situation ?", audio: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/paragraphe+2.wav", url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-1V2.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-2.mp4', choice1: 'Incertain, il demande à son collègue qu`il détaille sa méthode et la raison qui le motive à l’utiliser, une fois les instructions reçues, il partage l’opinion de son collègue et décide donc de procéder selon cette méthode qu’ils considèrent toutes les deux comme efficace et sécuritaire.', choice2: 'Après avoir demandé des précisions à son collègue et avoir fait la lecture de la fiche de cadenassage, il choisit de faire un compromis entre les deux méthodes. Il considère que prendre le meilleur des deux solutions lui garantira un bon résultat.', choice3: 'Ne pas écouter son collègue, prendre la fiche de cadenassage et appliquer la procédure comme indiqué par le règlement.', text: ["• Exercer son jugement", "• Écouter son collègue", "• Collaborer pour trouver la solution la plus sécuritaire", "• Valider auprès de son superviseur"]},
+  const chapter = [{audioAns: ["https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-1.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-2.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-3.mp3"], audioQuestion: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/1-0.wav", question: "Quel aurait été la meilleure attitude à adopter lors de la réunion du début de quart de travail ?", audio: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/paragraphe+1.wav", url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-2.mp4', choice1: 'Souhaitant clarifier ses incompréhensions rapidement, il intervient immédiatement dans le groupe afin de demander des précisions.', choice2: 'À la sortie, il demande au superviseur quand sera le bon moment pour lui parler avant la tâche. Le superviseur lui dit qu’il passera le voir rapidement. Il lui posera alors ses questions.', choice3: 'Il attend que la rencontre se termine et va valider les informations avec le superviseur lorsque tout le monde se dirige vers leur tâche à accomplir.', text: ["• Assurer votre sécurité et celle de vos collègues", "• Poser des questions", "• Choisir le bon moment", "• Favoriser le dialogue"]},
+                  {audioAns: ["https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-1.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-2.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-3.mp3"], audioQuestion: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/2-0.mp3", question: "Quel aurait été le meilleur comportement à adopter pour cette situation ?", audio: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/paragraphe+2.wav", url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-1V2.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-2.mp4', choice1: 'Incertain, il demande à son collègue qu’il détaille sa méthode et la raison qui le motive à l’utiliser, une fois les instructions reçues, il partage l’opinion de son collègue et décide donc de procéder selon cette méthode qu’ils considèrent tous les deux comme efficace et sécuritaire.', choice2: 'Après avoir demandé des précisions à son collègue et avoir fait la lecture de la fiche de cadenassage, il choisit de faire un compromis entre les deux méthodes. Il considère que prendre le meilleur des deux solutions lui garantira un bon résultat.', choice3: 'Ne pas écouter son collègue, prendre la fiche de cadenassage et appliquer la procédure indiquée dans le règlement.', text: ["• Exercer son jugement", "• Écouter son collègue", "• Collaborer pour trouver la solution la plus sécuritaire", "• Valider auprès de son superviseur"]},
                   {audioAns: ["https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/3-1.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/3-2.mp3", "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/3-3.mp3"], audioQuestion: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/3-0.mp3", question: "Qu'est que Jeff aurait pu faire pour éviter que son collègue se blesse ?", audio: "https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/paragraphe+3.wav", url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch3-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch3-2.mp4', choice1: 'Nouveau en poste, il ne se sent pas à l’aise d’intervenir. Sans aviser son collègue de la situation à risque, il part rapidement aviser son superviseur avant que son collègue vive une situation potentiellement dangereuse.', choice2: 'Il intervient rapidement auprès de son collègue et lui demande d’arrêter sa tâche immédiatement, il avise son collègue que la situation est dangereuse en expliquant les risques d’un tel problème.', choice3: 'Il avise son collègue du risque d’utiliser une meuleuse portative en lui indiquant qu’il ne voudrait pas que quelque chose de malheureux arrive.', text: ["• Signaler le danger", "• Choisir le bon moment"]}]
 
-  const selection = [{1: "Demander des précisions pendant la réunion préparatoire.", 2: "Demander de venir le superviseur voir rapidement pour des précisions.", 3: "Demander des renseignements au superviseur justes après la réunion préparatoire.", 4: 'Pas tout à fait...', 5: 'Bon choix!', 6: 'Pas tout à fait...'},
+  const selection = [{1: "Demander des précisions pendant la réunion préparatoire.", 2: "Demander de venir le superviseur voir rapidement pour des précisions.", 3: "Demander des renseignements au superviseur juste après la réunion préparatoire.", 4: 'Pas tout à fait...', 5: 'Bon choix!', 6: 'Pas tout à fait...'},
                     {1: "Considérer l’expérience de son collègue et suivre ses directives même si elles sont contraires à la procédure.", 2: "Prendre le meilleur des deux options.", 3: "Appliquer la procédure indiquer dans le règlement.", 4: 'Pas la bonne réponse...', 5: 'Pas tout à fait...', 6: 'Presque!'},
                     {1: "Aviser son superviseur sans prévenir le collègue.", 2: "Aviser son collègue et expliquer pourquoi c’est une situation à risque.", 3: "Aviser son collègue.", 4: 'Pas tout à fait...', 5: 'Presque!', 6: 'Mais encore...'}]
 
@@ -54,6 +54,16 @@ $(() => {
 
   const showQuestions = () => {
     $("#selector").css({display: "flex"})
+    $("#response").css({display: "none"})
+    $("html").fadeOut(0).fadeIn(1000)
+    audioQuestionRef.current.play()
+  }
+
+  const goBack = () => {
+    $("#video").css({display: "flex"})
+    $("#response").css({display: "none"})
+    $("#retry").css({display: "none"})
+    $("#retour").css({display: "none"})
     $("html").fadeOut(0).fadeIn(1000)
     audioQuestionRef.current.play()
   }
@@ -74,10 +84,12 @@ $(() => {
     $("#selector").css({display: "none"})
     $("#accueil").css({display: "block"})
     $("#retour").css({display: 'none'})
+    $("#retry").css({display: 'none'})
   }
 
   const showBtn = () => {
     $("#retour").css({display: 'block'})
+    $("#retry").css({display: 'block'})
   }
 
   const canHover = () => {
@@ -147,7 +159,10 @@ $(() => {
             <video src={chapter[id].urlMuted} autoPlay muted loop />
           </div>
         </div>
+        <div className='resetBtnContainers'>
             <button id="retour" className='retour' onClick={() => retour()}>Continuer</button>
+            <button id="retry"  className='retour' onClick={() => goBack()}>Réessayer</button>
+        </div>
       </div>
     </div>
   )
